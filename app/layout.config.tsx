87a1d536c8f60cc881d189b5ac17baa096079beb
burnedchris <christopher@everfund.com>
2025-05-14 15:07:36 +0200

    Initialize project structure with essential files and configurations. Added .gitignore, TypeScript configuration, and package.json. Implemented basic layout and routing for the application, including API documentation structure and OpenAPI integration. Included global styles and layout components for consistent design. Created initial documentation files for API usage.

---
+++ app/layout.config.tsx
@@ -0,0 +1,8 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { Logo } from '@/app/logo';
+
+export const baseOptions: BaseLayoutProps = {
+  nav: {
+    title: <Logo />,
+  },
+};
